c06422616d70200bef9ee1fb2e4ea727e33a956e
Sorachat <73382296+ter2002@users.noreply.github.com>
2022-03-24 18:38:50 +0700

    get one page

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const debug = require("debug")("app");
 const morgan = require("morgan");
 const app = express();
 const port = 3000;
+const path = require("path");
 
+app.use(express.static(path.join(__dirname, "/public/")))
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
