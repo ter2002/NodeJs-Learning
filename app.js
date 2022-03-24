002c95dc29449fe89f568d3cd821d4291216e304
Sorachat <73382296+ter2002@users.noreply.github.com>
2022-03-24 23:22:46 +0700

    add auto connect with nodemon

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "/public/")))
+app.use(express.static(path.join(__dirname, "/public/")));
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(port, () => {
-  console.log("Listening on port " + chalk.green(port));
+app.listen(PORT, () => {
+  console.log("Listening on PORT " + chalk.green(PORT));
 });
