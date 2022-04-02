a70b9bb7b25bc1246ba737ffac87264efc012709
Sorachat <73382296+ter2002@users.noreply.github.com>
2022-04-02 19:14:29 +0700

    .

--- app.js
+++ app.js
@@ -7,22 +7,14 @@ const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT;
 const path = require("path");
-const products = require("./data/products.json");
-const productRouter = express.Router();
-app.use(express.static(path.join(__dirname, "/public/")));
 
+app.use(express.static(path.join(__dirname, "/public/")));
+const productsRouter = require("./src/router/productsRouter");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 // app.get("/products/");
-productRouter.route("/").get((req, res) => {
-  res.render("products", products);
-});
 
-productRouter.route("/:id").get((req, res) => {
-  const id = req.params.id;
-  res.send("Hello I am Product " + id);
-});
-app.use("/products", productRouter);
+app.use("/products", productsRouter);
 app.get("/", (req, res) => {
   res.render("index", {
     username: "terzazaza",
